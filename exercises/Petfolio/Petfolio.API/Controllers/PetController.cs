03cbd6c9ff639175ec5d827f179b98563de22c2f
Jessé Bruno Correia <jesse.bcorreia@gmail.com>
2026-07-27 22:43:35 -0300

    /exercises/Petfolio | instala Scalar para visualizar a documentação da API

--- exercises/Petfolio/Petfolio.API/Controllers/PetController.cs
+++ exercises/Petfolio/Petfolio.API/Controllers/PetController.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Petfolio.Application.UseCases.Pet.Register;
+using Petfolio.Application.UseCases.Pets.Register;
 using Petfolio.Communication.Requests;
 using Petfolio.Communication.Responses;
 
