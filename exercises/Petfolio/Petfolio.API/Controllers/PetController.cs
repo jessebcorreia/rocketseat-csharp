646a1a36de3fd95ff913c52ee506c2a59530d0af
Jessé Bruno Correia <jesse.bcorreia@gmail.com>
2026-07-27 23:19:16 -0300

    /exercises/Petfolio | implementa classe de Errors

--- exercises/Petfolio/Petfolio.API/Controllers/PetController.cs
+++ exercises/Petfolio/Petfolio.API/Controllers/PetController.cs
@@ -12,6 +12,7 @@ public class PetController : ControllerBase
 {
     [HttpPost]
     [ProducesResponseType(typeof(ResponseRegisterPetJson), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(ResponseErrorsJson), StatusCodes.Status400BadRequest)]
     public IActionResult Register([FromBody] RequestPetJson request)
     {
         var registerPetUseCase = new RegisterPetUseCase();
@@ -22,6 +23,7 @@ public class PetController : ControllerBase
 
     [HttpPut("{id}")]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(typeof(ResponseErrorsJson), StatusCodes.Status400BadRequest)]
     public IActionResult Update([FromRoute] int id, [FromBody] RequestPetJson request)
     {
         var updatePetUseCase = new UpdatePetUseCase();
