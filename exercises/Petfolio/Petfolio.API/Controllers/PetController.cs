db7f559cfcf04f6b5d117cae89a9e3d0fe650528
Jessé Bruno Correia <jesse.bcorreia@gmail.com>
2026-07-27 23:11:22 -0300

    /exercises/Petfolio | implementa useCase e controller de edição de Pet

--- exercises/Petfolio/Petfolio.API/Controllers/PetController.cs
+++ exercises/Petfolio/Petfolio.API/Controllers/PetController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Petfolio.Application.UseCases.Pets.Register;
+using Petfolio.Application.UseCases.Pets.Update;
 using Petfolio.Communication.Requests;
 using Petfolio.Communication.Responses;
 
@@ -11,11 +12,21 @@ public class PetController : ControllerBase
 {
     [HttpPost]
     [ProducesResponseType(typeof(ResponseRegisterPetJson), StatusCodes.Status201Created)]
-    public IActionResult Register([FromBody] RequestRegisterPetJson request)
+    public IActionResult Register([FromBody] RequestPetJson request)
     {
         var registerPetUseCase = new RegisterPetUseCase();
         var response = registerPetUseCase.Execute(request);
 
         return Created(string.Empty, response);
+    }
+
+    [HttpPut("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public IActionResult Update([FromRoute] int id, [FromBody] RequestPetJson request)
+    {
+        var updatePetUseCase = new UpdatePetUseCase();
+        updatePetUseCase.Execute(id, request);
+
+        return NoContent();
     }
 }
