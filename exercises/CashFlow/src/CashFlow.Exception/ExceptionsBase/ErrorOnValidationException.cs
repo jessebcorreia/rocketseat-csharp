b712069cf4576fb957642afa7fe3fd96128add19
Jessé Bruno Correia <jesse.bcorreia@gmail.com>
2026-08-02 23:20:56 -0300

    /exercises/CashFlow | refatora o tratamento de exceções

--- exercises/CashFlow/src/CashFlow.Exception/ExceptionsBase/ErrorOnValidationException.cs
+++ exercises/CashFlow/src/CashFlow.Exception/ExceptionsBase/ErrorOnValidationException.cs
@@ -1,10 +1,20 @@
+using System.Net;
+
 namespace CashFlow.Exception.ExceptionsBase;
 
 public class ErrorOnValidationException : CashFlowException
 {
-    public List<string> ErrorMessages { get; set; }
+    private readonly List<string> _errors;
+
+    public override int StatusCode => (int)HttpStatusCode.BadRequest;
+
     public ErrorOnValidationException(List<string> errorMessages) : base(string.Empty)
     {
-        ErrorMessages = errorMessages;
+        _errors = errorMessages;
+    }
+
+    public override List<string> GetErrors()
+    {
+        return _errors;
     }
 }
