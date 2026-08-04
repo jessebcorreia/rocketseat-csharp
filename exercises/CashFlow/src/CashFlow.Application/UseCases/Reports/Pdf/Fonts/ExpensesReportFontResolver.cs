ad5bac78bb847adbfd47c36fe9dfd673f9795ed2
Jessé Bruno Correia <jesse.bcorreia@gmail.com>
2026-08-03 23:18:29 -0300

    /exercises/CashFlow | cria FontResolver

---
+++ exercises/CashFlow/src/CashFlow.Application/UseCases/Reports/Pdf/Fonts/ExpensesReportFontResolver.cs
@@ -0,0 +1,15 @@
+using PdfSharp.Fonts;
+namespace CashFlow.Application.UseCases.Reports.Pdf.Fonts;
+
+public class ExpensesReportFontResolver : IFontResolver
+{
+    public byte[]? GetFont(string faceName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public FontResolverInfo? ResolveTypeface(string familyName, bool bold, bool italic)
+    {
+        return new FontResolverInfo(familyName);
+    }
+}
