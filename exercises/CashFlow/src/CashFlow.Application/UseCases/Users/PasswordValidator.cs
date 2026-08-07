cd8251c4724843a1d35b4af53617a06bfdd15f5b
Jessé Bruno Correia <jesse.bcorreia@gmail.com>
2026-08-07 00:04:06 -0300

    /exercises/CashFlow | refatora o validador de senhas

--- exercises/CashFlow/src/CashFlow.Application/UseCases/Users/PasswordValidator.cs
+++ exercises/CashFlow/src/CashFlow.Application/UseCases/Users/PasswordValidator.cs
@@ -1,4 +1,3 @@
-using CashFlow.Exception;
 using FluentValidation;
 using FluentValidation.Validators;
 using System.Text.RegularExpressions;
@@ -8,6 +7,7 @@ namespace CashFlow.Application.UseCases.Users;
 public partial class PasswordValidator<T> : PropertyValidator<T, string>
 {
     private const string ERROR_MESSAGE_KEY = "ErrorMessage";
+
     public override string Name => "PasswordValidator";
 
     protected override string GetDefaultMessageTemplate(string errorCode)
@@ -17,23 +17,38 @@ public partial class PasswordValidator<T> : PropertyValidator<T, string>
 
     public override bool IsValid(ValidationContext<T> context, string password)
     {
-        if (IsValidPassword(password) == false)
+        var errorMessage = ValidatePassword(password);
+
+        if (errorMessage is not null)
         {
-            context.MessageFormatter.AppendArgument(ERROR_MESSAGE_KEY, ResourceErrorMessages.INVALID_PASSWORD);
+            context.MessageFormatter.AppendArgument(ERROR_MESSAGE_KEY, errorMessage);
             return false;
         }
 
         return true;
     }
 
-    private static bool IsValidPassword(string password)
+    private static string? ValidatePassword(string password)
     {
-        return !string.IsNullOrWhiteSpace(password)
-            && password.Length >= 8
-            && UppercaseLetter().IsMatch(password)
-            && LowercaseLetter().IsMatch(password)
-            && SpecialCharacter().IsMatch(password)
-            && Number().IsMatch(password);
+        if (string.IsNullOrWhiteSpace(password))
+            return "Password is required.";
+
+        if (password.Length < 8)
+            return "Password must be at least 8 characters long.";
+
+        if (!UppercaseLetter().IsMatch(password))
+            return "Password must contain at least one uppercase letter.";
+
+        if (!LowercaseLetter().IsMatch(password))
+            return "Password must contain at least one lowercase letter.";
+
+        if (!Number().IsMatch(password))
+            return "Password must contain at least one number.";
+
+        if (!SpecialCharacter().IsMatch(password))
+            return "Password must contain at least one special character.";
+
+        return null;
     }
 
     [GeneratedRegex(@"[A-Z]")]
@@ -42,7 +57,7 @@ public partial class PasswordValidator<T> : PropertyValidator<T, string>
     [GeneratedRegex(@"[a-z]")]
     private static partial Regex LowercaseLetter();
 
-    [GeneratedRegex(@"[!?.]")]
+    [GeneratedRegex(@"[!?.@]")]
     private static partial Regex SpecialCharacter();
 
     [GeneratedRegex(@"[0-9]")]
