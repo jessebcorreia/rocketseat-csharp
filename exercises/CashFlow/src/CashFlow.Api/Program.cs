b4bd3a487c101dab3a00d471bfe393393162f091
Jessé Bruno Correia <jesse.bcorreia@gmail.com>
2026-08-09 22:19:25 -0300

    /exercises/CashFlow | implementa classe para recuperar token do usuário logado

--- exercises/CashFlow/src/CashFlow.Api/Program.cs
+++ exercises/CashFlow/src/CashFlow.Api/Program.cs
@@ -1,6 +1,8 @@
 using CashFlow.Api.Filters;
 using CashFlow.Api.Middleware;
+using CashFlow.Api.Token;
 using CashFlow.Application;
+using CashFlow.Domain.Security.Tokens;
 using CashFlow.Infrastructure;
 using CashFlow.Infrastructure.Extensions;
 using CashFlow.Infrastructure.Migrations;
@@ -22,9 +24,11 @@ builder.Services.AddOpenApi(options =>
 
 builder.Services.AddRouting(option => option.LowercaseUrls = true);
 builder.Services.AddMvc(options => options.Filters.Add<ExceptionFilter>());
-
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
+
+builder.Services.AddScoped<ITokenProvider, HttpContextTokenValue>();
+builder.Services.AddHttpContextAccessor();
 
 AddAuthentication();
 
--- exercises/CashFlow/src/CashFlow.Infrastructure/DependencyInjectionExtension.cs
+++ exercises/CashFlow/src/CashFlow.Infrastructure/DependencyInjectionExtension.cs
@@ -3,10 +3,12 @@ using CashFlow.Domain.Repositories.Expenses;
 using CashFlow.Domain.Repositories.Users;
 using CashFlow.Domain.Security.Cryptography;
 using CashFlow.Domain.Security.Tokens;
+using CashFlow.Domain.Services.LoggedUser;
 using CashFlow.Infrastructure.DataAccess;
 using CashFlow.Infrastructure.DataAccess.Repositories;
 using CashFlow.Infrastructure.Extensions;
 using CashFlow.Infrastructure.Security.Tokens;
+using CashFlow.Infrastructure.Services.LoggedUser;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -56,5 +58,6 @@ public static class DependencyInjectionExtension
         var signingKey = configuration.GetValue<string>("Settings:Jwt:SigningKey");
 
         services.AddScoped<IAccessTokenGenerator>(config => new JwtTokenGenerator(expirationMinutes, signingKey!));
+        services.AddScoped<ILoggedUser, LoggedUser>();
     }
 }
